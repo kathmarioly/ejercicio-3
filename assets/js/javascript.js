9b0c98cf01f69de1672975246e54eba1f18ca4c7
kathmarioly <katerinemarioly@gmail.com>
2017-04-24 23:09:49 -0300

    Modificando js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,7 @@
+function convertirCelsius(celsius){ 
 
+   var farenheit = document.getElementById("celsius").value;
 
-var celcius = [];
-var farenheit = celcius.map(function(value){
-    return Math.round((value *9)/5 + 32)
-  }); 
-//Con map estoy recorriendo cada uno de los elementos del array celcius, y en la variable transformacion, estoy haciendo la conversion del array C° a F° mediante la formula de conversión.
-document.write(document.getElementById("farenheit").value);
+    document.write( Math.round((5/9) * (farenheit - 32)));
+
+}
