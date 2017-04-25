ab898e77f57ce2bfbf2504202363b98c51c758f8
kathmarioly <katerinemarioly@gmail.com>
2017-04-24 22:43:08 -0300

    Creación de botones

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,8 @@
-alert("the worsttttttt is coming");
+
+
+var celcius = [];
+var farenheit = celcius.map(function(value){
+    return Math.round((value *9)/5 + 32)
+  }); 
+//Con map estoy recorriendo cada uno de los elementos del array celcius, y en la variable transformacion, estoy haciendo la conversion del array C° a F° mediante la formula de conversión.
+document.write(document.getElementById("farenheit").value);
